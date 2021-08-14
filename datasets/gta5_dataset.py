edb2a02be388045fd23219d072fd7483020f1700
DreamLens <DreamLen98@proton.me>
2021-08-14 06:34:08 +0000

    gradlew

--- datasets/gta5_dataset.py
+++ datasets/gta5_dataset.py
@@ -3,4 +3,12 @@ import os.path as osp
 import numpy as np
 import random
 import collections
-im
+import torch
+import torchvision
+from torch.utils import data
+import cv2
+from PIL import Image
+
+
+class GTA5DataSet(data.Dataset):
+    def __init__(self, root, list_path, max_iters=None, img_size=(321, 321), norm=False, random
