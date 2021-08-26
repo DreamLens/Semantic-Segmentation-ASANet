4485327c443a9889ef6d11a3e8b117f9b2a26c01
DreamLens <DreamLen98@proton.me>
2021-08-26 10:46:01 +0000

    code style fix

--- datasets/gta5_dataset.py
+++ datasets/gta5_dataset.py
@@ -11,4 +11,9 @@ from PIL import Image
 
 
 class GTA5DataSet(data.Dataset):
-    def __init__(self, root, list_path, max_iters=None, img_size=(321, 321), norm=False, random
+    def __init__(self, root, list_path, max_iters=None, img_size=(321, 321), norm=False, random_mirror=False, random_crop=False, ignore_label=255):
+        self.root = root
+        self.list_path = list_path
+        self.img_size = img_size
+        # self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
+        self.img_ids
