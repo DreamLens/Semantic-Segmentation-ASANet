44caa2d3614d2a96ae5bb959284a07e8d362bae3
DreamLens <DreamLen98@proton.me>
2023-02-02 19:33:15 +0000

    warnings

--- evaluate.py
+++ evaluate.py
@@ -17,4 +17,13 @@ from torch.autograd import Variable
 import torchvision.models as models
 
 from networks.deeplab import Deeplab_Res101
-from networks.deeplab_vgg import Deep
+from networks.deeplab_vgg import DeeplabVGG
+from networks.fcn8s import VGG16_FCN8s
+from datasets.cityscapes_dataset import cityscapesDataSet
+import timeit
+BACKBONE = 'resnet'
+IGNORE_LABEL = 255
+NUM_CLASSES = 19
+LOG_DIR = './logs'
+DATA_DIRECTORY = '/path/to/cityscapes'
+DA
