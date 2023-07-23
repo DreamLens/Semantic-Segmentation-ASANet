8c571e38e13a9e824161aac9a89e31c8951a3445
DreamLens <DreamLen98@proton.me>
2023-07-23 15:32:48 +0000

    new settings

--- networks/deeplab_vgg.py
+++ networks/deeplab_vgg.py
@@ -13,4 +13,11 @@ class Classifier_Module(nn.Module):
             self.conv2d_list.append(nn.Conv2d(
                 dims_in, num_classes, kernel_size=3, stride=1, padding=padding, dilation=dilation, bias=True))
 
-        for m in self.conv2d_list
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.conv2d_list[0](x)
+        for i in range(len(self.conv2d_list)-1):
+            out += self.conv2d_list[i+1](x)
+           
