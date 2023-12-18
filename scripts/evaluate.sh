eca78c6e999852a65ee77419c41591099e3caa4c
DreamLens <DreamLen98@proton.me>
2023-12-18 22:48:30 +0000

    Cleaning up...

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,4 +3,9 @@ ls
 export PYTHONPATH=`pwd`
 CUDA_VISIBLE_DEVICES=0 python evaluate.py --backbone='resnet'\
         --data-dir='/path/to/cityscapes' \
-        --l
+        --list-path='./datasets/cityscapes_list/val.txt' \
+        --log-dir=logs/GTA5ASA \
+        --restore-from='pretrained/GTA5ASA_45.10.pth'
+
+CUDA_VISIBLE_DEVICES=0 python evaluate.py --backbone='resnet'\
+        --data-dir='/path/to/cityscapes
