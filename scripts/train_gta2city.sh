7eff7bba2a6d1bbe7e794654c5e2a5fe1d54d86f
DreamLens <DreamLen98@proton.me>
2024-01-24 18:35:18 +0000

    Close #5400 - Handle dynamic beans for the documentation generation

--- scripts/train_gta2city.sh
+++ scripts/train_gta2city.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 set -ex
 ls
-export PYTHONP
+export PYTHONPATH=`pwd`
+DATA_DIR='/data/'
+SNAPSHOTS_DIR='../snapshots/'
+echo ${DATA_DIR}
+CUDA_VISIBLE_DEVICES=0 python train_gta2city.py \
+            --method='GTA5KLASA' \
+            --backbone='resnet'\
+            --data-dir=${DATA_DIR}g
