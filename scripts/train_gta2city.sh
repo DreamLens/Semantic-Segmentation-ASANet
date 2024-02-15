8082be69888837b1fac8a2dd2e991e318d7db53f
DreamLens <DreamLen98@proton.me>
2024-02-15 18:04:52 +0000

    dm thin: reduce endio_hook pool size

--- scripts/train_gta2city.sh
+++ scripts/train_gta2city.sh
@@ -8,4 +8,8 @@ echo ${DATA_DIR}
 CUDA_VISIBLE_DEVICES=0 python train_gta2city.py \
             --method='GTA5KLASA' \
             --backbone='resnet'\
-            --data-dir=${DATA_DIR}g
+            --data-dir=${DATA_DIR}gta5 \
+            --data-list='./datasets/gta5_list/train.txt' \
+            --data-dir-target=${DATA_DIR}cityscapes \
+            --data-list-target='./datasets/cityscapes_list/train.txt' \
+            --snapshot-dir=${SNAPSHOTS_DIR} \
